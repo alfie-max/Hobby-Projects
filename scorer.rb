7513a3078c02fcda92145882d11af3a3d6983a1a
alfie <alfie.2012@gmail.com>
2015-03-28 00:47:46 +0530

    create separate methods

--- scorer.rb
+++ scorer.rb
@@ -3,6 +3,25 @@
 require 'open-uri'
 require 'nokogiri'
 
+def notify(message)
+  command = 'notify-send ' + '"' + message + '"'
+  system(command)
+end
+
+def byebye
+  puts "\b\bBye Bye... See you for the next match"
+end
+
+def take_a_break(seconds)
+  begin
+    sleep(seconds)
+  rescue SystemExit, Interrupt
+    byebye
+    raise SystemExit
+  end
+end
+
+
 url = "http://static.cricinfo.com/rss/livescores.xml"
 
 begin
@@ -11,21 +30,15 @@ begin
     if open_url.status[0].to_i == 200
       data = Nokogiri::XML(open_url.read)
       desc = data.xpath('//description')
-      command = 'notify-send ' + '"' + desc[1].content + '"'
-      system(command)
+      notify desc[1].content
     end
-    sleep(5)
+    take_a_break 5
   end
 rescue SystemExit, Interrupt
-  puts "\b\bBye Bye... See you for the next match"
+  byebye
 rescue Exception => error
-  message = "Failed to fetch the score:  #{error}"
-  command = 'notify-send ' + '"' + message + '"'
-  begin
-    system(command)
-    sleep(15)
-    retry
-  rescue SystemExit, Interrupt
-    puts "\b\bBye Bye... See you for the next match"
-  end
+  err_msg = "Failed to fetch the score:  #{error}"
+  notify err_msg
+  take_a_break 15
+  retry
 end
